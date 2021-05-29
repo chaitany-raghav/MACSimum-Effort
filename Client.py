dabd4b9d673751c954eb8410caeb84eaa09e43d2
chaitany-raghav <chaitany_r@ee.iitr.ac.in>
2021-05-29 13:34:12 +0530

    A working model
    YAYA,it wokrs

--- Client.py
+++ Client.py
@@ -1,85 +1,41 @@
 import pygame as pg
 from Network import Network
+from Player import *
+import Config
 
-width = 500
-height = 500
+width = Config.WindowWidth
+height = Config.WindowHeight
 
 win = pg.display.set_mode((width,height))
 pg.display.set_caption("Client")
 
-FPS=60
 
-clientNumber = 0
+PlayerID=-1
 
-class Player():
-    def __init__(self,x,y,width,height,colour):
-        self.x=x
-        self.y=y
-        self.width=width
-        self.height=height
-        self.colour=colour
-        self.rect=(x,y,width,height)
-        self.vel=3
-
-    def draw(self,win):
-        pg.draw.rect(win,self.colour,self.rect)
-
-    def move(self):
-        keys=pg.key.get_pressed()
-
-        if keys[pg.K_LEFT]:
-            self.x-=self.vel
-
-        if keys[pg.K_RIGHT]:
-            self.x+=self.vel
-
-        if keys[pg.K_UP]:
-            self.y-=self.vel
-
-        if keys[pg.K_DOWN]:
-            self.y+=self.vel
-        
-        self.update()
-
-    def update(self):
-        self.rect=(self.x,self.y,self.width,self.height)
-
-def read_pos(str):
-    str =str.split(',')
-    return int(str[0]),int(str[1])
-
-def make_pos(tup):
-    return str(tup[0])+","+str(tup[1])
-
-def redrawWindow(win,player,player2):
+def redrawWindow(win,players):
 
     win.fill((255,255,255))
-    player.draw(win)
-    player2.draw(win)
+    for p in players:
+        p.draw(win)
     pg.display.update()
 
 
 def main():
     run=True
     n=Network()
-    startPos = read_pos(n.getPos())
-    p=Player(startPos[0],startPos[1],100,100,(0,0,255))
-    p2=Player(0,0,100,100,(0,0,255))
+    p=n.getP()
+    global PlayerID
+    PlayerID=p.id
     clock=pg.time.Clock()
-
     while run:
-        clock.tick(FPS)
-        p2Pos=read_pos(n.send(make_pos((p.x,p.y))))
-        p2.x=p2Pos[0]
-        p2.y=p2Pos[1]
-        p2.update()
-
+        clock.tick(Config.FPS)
+        data=n.send(p)
         for event in pg.event.get():
             if event.type ==pg.QUIT:
                 run =False
                 pg.quit()
         
         p.move()
-        redrawWindow(win,p,p2)
+        redrawWindow(win,data)
 
 main()
--- Server.py
+++ Server.py
@@ -1,12 +1,15 @@
 import socket
 from _thread import *
 import sys
+import Config
+from Player import Player
+import pickle
 
 #The ip of the server
-server = "10.0.0.2"
-port = 5555
+server = Config.ServerIP
+port = Config.ServerPort
 
-maxPlayerCount=2
+currentPlayer = 0
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
@@ -15,47 +18,42 @@ try:
 except socket.error as e:
     print(e)
  
-s.listen(maxPlayerCount)
+s.listen(Config.maxPlayerCount)
 print("Wating for a connection,Server Started")
 
-def read_pos(str):
-    str =str.split(',')
-    return int(str[0]),int(str[1])
 
-def make_pos(tup):
-    return str(tup[0])+","+str(tup[1])
-
-pos=[(0,0),(100,100)]
+pos=[
+    Player(Config.PlayerOffSet,(Config.WindowHeight-Config.PlayerLength)/2,Config.PlayerWidth,Config.PlayerLength,0,(255,0,0)),
+    Player(Config.WindowWidth-Config.PlayerWidth-Config.PlayerOffSet,(Config.WindowHeight-Config.PlayerLength)/2,Config.PlayerWidth,Config.PlayerLength,1,(0,255,0)),
+    Player((Config.WindowWidth-Config.PlayerLength)/2,Config.PlayerOffSet,Config.PlayerLength,Config.PlayerWidth,2,(0,0,255)),
+    Player((Config.WindowWidth-Config.PlayerLength)/2,Config.WindowHeight-Config.PlayerWidth-Config.PlayerOffSet,Config.PlayerLength,Config.PlayerWidth,3,(128,128,0))
+    ]
 
 def threaded_client(conn,Player):
-    conn.send(str.encode(make_pos(pos[Player])))
-    reply=""
+    conn.send(pickle.dumps(pos[Player]))
     while True:
         try:
-            data=read_pos(conn.recv(2048).decode())
+            data=pickle.loads(conn.recv(2048))
             pos[Player]=data
             if not data:
                 print("Disconnected")
                 break
             else:
-                if Player==1:
-                    reply=pos[0]
-                else:
-                    reply=pos[1]
-
+                reply=pickle.dumps(pos)
                 print("Recived:",data)
                 print("Sending:",reply) 
 
-            conn.sendall(str.encode(make_pos(reply)))
+            conn.sendall(reply)
         except:
             break
-    
+    global currentPlayer
+    currentPlayer-=1
     print("Lost connection")
     conn.close()
 
-currestPlayer = 0
+
 while True:
     conn,addr=s.accept()
     print("Connecting to:",addr)
-    start_new_thread(threaded_client,(conn,currestPlayer))
-    currestPlayer+=1
+    start_new_thread(threaded_client,(conn,currentPlayer))
+    currentPlayer+=1
