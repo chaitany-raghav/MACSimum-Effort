6c2ad03d4dbbdc55d1b69131c138d428f78ec03e
chaitany-raghav <chaitany_r@ee.iitr.ac.in>
2021-05-29 13:35:46 +0530

    It works
    Added 4 player support over the LAN

---
+++ Config.py
@@ -0,0 +1,15 @@
+ServerIP="10.0.0.2"
+ServerPort=5555
+
+PlayerSpeed=3
+
+WindowHeight=500
+WindowWidth=500
+PlayerLength=70
+PlayerWidth=10
+
+PlayerOffSet=30
+
+FPS=60
+
+maxPlayerCount=4
---
+++ Player.py
@@ -0,0 +1,39 @@
+import pygame as pg
+import Config
+
+class Player():
+    def __init__(self,x,y,width,height,id,colour):
+        self.x=x
+        self.y=y
+        self.width=width
+        self.height=height
+        self.colour=colour
+        self.rect=(x,y,width,height)
+        self.id=id
+        self.vel=Config.PlayerSpeed
+
+
+    def draw(self,win):
+        pg.draw.rect(win,self.colour,self.rect)
+
+    def move(self):
+        keys=pg.key.get_pressed()
+
+        if self.height>self.width:
+            if keys[pg.K_UP]:
+                self.y-=self.vel
+
+            if keys[pg.K_DOWN]:
+                self.y+=self.vel
+        else:
+            if keys[pg.K_LEFT]:
+                self.x-=self.vel
+
+            if keys[pg.K_RIGHT]:
+                self.x+=self.vel
+
+        
+        self.update()
+
+    def update(self):
+        self.rect=(self.x,self.y,self.width,self.height)
